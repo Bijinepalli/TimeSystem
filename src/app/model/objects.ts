b78e62e55fd86fd7b9742ea36072f986fd492d22
Dharmatej Nandikanti <dharmatej.nandikanti@pbsystems.biz>
2018-11-22 16:06:24 +0530

    Login Changes

--- src/app/model/objects.ts
+++ src/app/model/objects.ts
@@ -5,3 +5,45 @@ export class Holidays {
     CalendarYear: number;
     InUse: number;
 }
+
+export class AppSettings {
+    Id?: number;
+    DataKey?: string;
+    Value?: string;
+    Description?: string;
+    Type?: string;
+}
+
+export class Employee {
+    ID?: string;
+    UserLevel?: string;
+    LoginID?: string;
+    FirstName?: string;
+    LastName?: string;
+    Password?: string;
+    NickName?: string;
+    EmailAddress?: string;
+    Inactive?: string;
+    Salaried?: string;
+    SubmitsTime?: string;
+    PayAvailableAlert?: string;
+    IPayEligible?: string;
+    Officer?: string;
+    HireDate?: string;
+    TimeStamp?: string;
+    CompanyHolidays?: string;
+    HoursPerDay?: string;
+    SSMA_TimeStamp?: string;
+    SecondaryEmailAddress?: string;
+    PasswordUpdatedOn?: string;
+    IsLocked?: string;
+    NoOfAttempts?: string;
+    FailureAttemptOn?: string;
+    PasswordExpiresOn?: string;
+    TerminationDate?: string;
+    StartDate?: string;
+}
+
+export class LoginErrorMessage {
+    ErrorMessage?: string;
+}
--- src/app/service/timesystem.service.ts
+++ src/app/service/timesystem.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import {
-  Holidays
+  Holidays, AppSettings, Employee, LoginErrorMessage
 } from '../model/objects';
 import { Observable, forkJoin } from 'rxjs';
 
@@ -10,7 +10,8 @@ import { Observable, forkJoin } from 'rxjs';
 })
 export class TimesystemService {
 
-  private ipaddress = 'http://172.16.32.67/';
+
+  private ipaddress = 'http://172.16.32.57/';
   private url = this.ipaddress + 'TimeSystemService/';
   constructor(private http: HttpClient) { }
 
@@ -24,4 +25,33 @@ export class TimesystemService {
   getHelp() {
     return this.http.get('http://172.16.32.67/ECTS/TimeSystem/help/HolidayUpdate.htm');
   }
+
+  getAppSettings(): any {
+    const params = new HttpParams();
+    return this.http.get<AppSettings[]>(this.url + 'GetAppSettings', { params });
+  }
+
+  getEmployee(EmployeeID: string, LoginID: string, Password: string): any {
+    const params = new HttpParams()
+      .set('EmployeeID', EmployeeID !== '' ? EmployeeID : '0')
+      .set('LoginID', LoginID)
+      .set('Password', Password);
+    return this.http.get<Employee[]>(this.url + 'GetEmployee', { params });
+  }
+
+  EmployeeValidateByLoginID(LoginID: string): any {
+    const params = new HttpParams()
+      .set('LoginID', LoginID);
+    return this.http.get<LoginErrorMessage[]>(this.url + 'EmployeeValidateByLoginID', { params });
+  }
+
+  EmployeeValidateByCredentials(AttemptsLimit: string, LoginID: string, Password: string): any {
+    const params = new HttpParams()
+      .set('LoginID', LoginID)
+      .set('Password', Password)
+      .set('AttemptsLimit', AttemptsLimit);
+    return this.http.get<LoginErrorMessage[]>(this.url + 'EmployeeValidateByCredentials', { params });
+  }
+
+
 }
