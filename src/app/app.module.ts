9f739e60dd21f6e3341817387f04fee82ac44824
SitaramaRaju Kakarlapudi <sitharam.k@pbsystems.biz>
2018-12-04 14:02:52 +0530

    change service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -153,7 +153,8 @@ const appRoutes: Routes = [
     ListemployeesreportsComponent,
     ForgotpasswordComponent,
     ChangepasswordComponent,
-    EmployeelogindataComponent
+    EmployeelogindataComponent,
+    EmployeesbybillingcodeComponent,
   ],
   imports: [
     BrowserAnimationsModule,
