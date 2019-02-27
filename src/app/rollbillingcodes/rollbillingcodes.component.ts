ccfed471f3f6ac3ca56c5f727f7a3a20deb6bdaa
Sudheer Bijinepalli <Sudheer@pbsystems.biz>
2019-02-27 11:04:40 +0530

    DateFormat

--- src/app/rollbillingcodes/rollbillingcodes.component.ts
+++ src/app/rollbillingcodes/rollbillingcodes.component.ts
@@ -13,6 +13,8 @@ import { TimeSheet } from '../model/objects';
   styleUrls: ['./rollbillingcodes.component.css']
 })
 export class RollbillingcodesComponent implements OnInit {
+  visibleHelp: boolean;
+  helpText: string;
 
   constructor(private timesysSvc: TimesystemService, private router: Router, private msgSvc: MessageService,
     private confSvc: ConfirmationService, private activatedRoute: ActivatedRoute,
@@ -56,6 +58,23 @@ export class RollbillingcodesComponent implements OnInit {
     this.rollBillingForm.addControl('txtHolidayName', new FormControl('', null));
     this.rollBillingForm.addControl('txtPTOCode', new FormControl('', null));
     this.rollBillingForm.addControl('txtPTOName', new FormControl('', null));
+  }
+
+  Approve() {
+
+  }
+
+  showHelp(file: string) {
+    this.timesysSvc.getHelp(file)
+      .subscribe(
+        (data) => {
+          // this.helpText = data;
+          this.visibleHelp = true;
+          const parser = new DOMParser();
+          const parsedHtml = parser.parseFromString(data, 'text/html');
+          this.helpText = parsedHtml.getElementsByTagName('body')[0].innerHTML;
+        }
+      );
   }
 
 }
--- src/app/sharedpipes/dateformat.ts
+++ src/app/sharedpipes/dateformat.ts
@@ -7,6 +7,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateTimeFormatPipe extends DatePipe implements PipeTransform {
     transform(datevalue: any, format: any): any {
-        return super.transform(datevalue, format);
+        // tslint:disable-next-line:radix
+        const dt = new Date(parseInt(datevalue.substr(6)));
+        return super.transform(dt, format);
+       /* const a = /\/Date\((\d*)\)\//.exec(datevalue);
+        if (a) {
+            return super.transform(new Date(+a[1]), format);
+        }*/
     }
 }
