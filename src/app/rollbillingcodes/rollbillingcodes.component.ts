afadc6fc0a8fbe61cf5c4e63655fb224168987c3
Yohit <yohitps@gmail.com>
2019-03-19 16:09:11 +0530

    roll

--- src/app/rollbillingcodes/rollbillingcodes.component.ts
+++ src/app/rollbillingcodes/rollbillingcodes.component.ts
@@ -6,6 +6,7 @@ import { DatePipe, DecimalPipe } from '@angular/common';
 import { TimesystemService } from '../service/timesystem.service';
 import { CommonService } from '../service/common.service';
 import { TimeSheet } from '../model/objects';
+import { YearEndCodes } from '../model/constants';
 
 @Component({
   selector: 'app-rollbillingcodes',
@@ -21,6 +22,7 @@ export class RollbillingcodesComponent implements OnInit {
     private fb: FormBuilder, private commonSvc: CommonService) { }
 
   _showRoll = false;
+  _addNew = false;
   _action1 = '';
   _action2 = '';
   selectedValue = 0;
@@ -28,46 +30,51 @@ export class RollbillingcodesComponent implements OnInit {
   _upComingYear = '';
   _priorYear = '';
   ngOnInit() {
-    const jsonDate = '/Date(1517855400000+0530)/ ';
-    const onlyDate = +(jsonDate.match(/\d+/)[0]);
-    const getDate = new Date(onlyDate);
-    console.log(getDate);
+    // const jsonDate = '/Date(1517855400000+0530)/ ';
+    // const onlyDate = +(jsonDate.match(/\d+/)[0]);
+    // const getDate = new Date(onlyDate);
     this.addDefaultControlsToForm();
-    const todayDate = new Date();
-    console.log((todayDate.getFullYear() + 1).toString());
-    this._upComingYear = todayDate.getMonth() === 12 ? (todayDate.getFullYear() + 1).toString() : todayDate.getFullYear().toString();
-    this._priorYear = todayDate.getMonth() === 12 ? todayDate.getFullYear().toString() : (todayDate.getFullYear() - 1).toString();
-    console.log(todayDate.getMonth() + '-month');
-    console.log(todayDate.getDate() + '-day');
-    if ((todayDate.getMonth() === 12 && todayDate.getDate() > 15) || todayDate.getMonth() === 1) {
+    const todayDate = new Date('2019-01-19');
+    console.log(todayDate);
+    console.log(todayDate.getMonth());
+    this._upComingYear = todayDate.getMonth() === 11 ? (todayDate.getFullYear() + 1).toString() : todayDate.getFullYear().toString();
+    this._priorYear = todayDate.getMonth() === 11 ? todayDate.getFullYear().toString() : (todayDate.getFullYear() - 1).toString();
+    if ((todayDate.getMonth() === 11 && todayDate.getDate() > 15) || todayDate.getMonth() === 0) {
       // tslint:disable-next-line:max-line-length
-      this._action1 = 'Add New Non-Billable Codes for ' + (todayDate.getMonth() === 12 ? (todayDate.getFullYear() + 1) : todayDate.getFullYear());
+      this._action1 = 'Add New Non-Billable Codes for ' + (todayDate.getMonth() === 11 ? (todayDate.getFullYear() + 1) : todayDate.getFullYear());
       // tslint:disable-next-line:max-line-length
-      this._action2 = 'Inactivate Non-Billable Codes for ' + (todayDate.getMonth() === 12 ? todayDate.getFullYear() : (todayDate.getFullYear() - 1));
+      this._action2 = 'Inactivate Non-Billable Codes for ' + (todayDate.getMonth() === 11 ? todayDate.getFullYear() : (todayDate.getFullYear() - 1));
+      this.rollBillingForm.enable();
+    } else {
+      // tslint:disable-next-line:max-line-length
+      this._action1 = 'Add New Non-Billable Codes';
+      // tslint:disable-next-line:max-line-length
+      this._action2 = 'Inactivate Non-Billable Codes';
       this._showRoll = true;
       this.selectedValue = 0;
       this.rollBillingForm.disable();
-    } else {
-      this.rollBillingForm.enable();
     }
   }
   onradioChange(value: string) {
     this._NonBillSelect = true;
+    const yearEndCodes = new YearEndCodes();
     if (value === '0') {
+      this._addNew = true;
       this.commonSvc.getAppSettingsValue('TimeSheetIntimationCountDown');
-      this.rollBillingForm.controls['txtBenchCode'].setValue('');
-      this.rollBillingForm.controls['txtBenchName'].setValue('');
-      this.rollBillingForm.controls['txtHolidayCode'].setValue('');
-      this.rollBillingForm.controls['txtHolidayName'].setValue('');
-      this.rollBillingForm.controls['txtPTOCode'].setValue('');
-      this.rollBillingForm.controls['txtPTOName'].setValue('');
+      this.rollBillingForm.controls['txtBenchCode'].setValue(yearEndCodes.BenchCode + this._upComingYear);
+      this.rollBillingForm.controls['txtBenchName'].setValue(yearEndCodes.BenchName + this._upComingYear);
+      this.rollBillingForm.controls['txtHolidayCode'].setValue(yearEndCodes.HolidayCode + this._upComingYear);
+      this.rollBillingForm.controls['txtHolidayName'].setValue(yearEndCodes.HolidayName + this._upComingYear);
+      this.rollBillingForm.controls['txtPTOCode'].setValue(yearEndCodes.PTOCode + this._upComingYear);
+      this.rollBillingForm.controls['txtPTOName'].setValue(yearEndCodes.PTOName + this._upComingYear);
     } else {
-      this.rollBillingForm.controls['txtBenchCode'].setValue('');
-      this.rollBillingForm.controls['txtBenchName'].setValue('');
-      this.rollBillingForm.controls['txtHolidayCode'].setValue('');
-      this.rollBillingForm.controls['txtHolidayName'].setValue('');
-      this.rollBillingForm.controls['txtPTOCode'].setValue('');
-      this.rollBillingForm.controls['txtPTOName'].setValue('');
+      this._addNew = false;
+      this.rollBillingForm.controls['txtBenchCode'].setValue(yearEndCodes.BenchCode + this._priorYear);
+      this.rollBillingForm.controls['txtBenchName'].setValue(yearEndCodes.BenchName + this._priorYear);
+      this.rollBillingForm.controls['txtHolidayCode'].setValue(yearEndCodes.HolidayCode + this._priorYear);
+      this.rollBillingForm.controls['txtHolidayName'].setValue(yearEndCodes.HolidayName + this._priorYear);
+      this.rollBillingForm.controls['txtPTOCode'].setValue(yearEndCodes.PTOCode + this._priorYear);
+      this.rollBillingForm.controls['txtPTOName'].setValue(yearEndCodes.PTOName + this._priorYear);
     }
 
   }
@@ -84,7 +91,10 @@ export class RollbillingcodesComponent implements OnInit {
     this.rollBillingForm.addControl('txtPTOName', new FormControl('', null));
   }
 
-  Approve() {
+  AddNewBC() {
+
+  }
+  IncativateOldBC() {
 
   }
 
