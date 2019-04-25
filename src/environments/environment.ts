ad71e78384ac0ad59ff849f65c6d0c9aa3a5a708
Sudheer Bijinepalli <Sudheer@pbsystems.biz>
2019-04-25 10:41:50 +0530

    ip change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   accessSystemURL: 'http://172.16.36.32/AccessSystem/',
   helpipaddress: 'http://172.16.32.67/ECTS/TimeSystem/Help/',
-  url: 'http://localhost/TimeSystemService/',
-  SOWFiles: 'http://172.16.32.67/SOWFiles/',
+  url: 'http://172.16.36.32/TimeSystemService/',
+  SOWFiles: 'http://172.16.36.32/SOWFiles/',
   buildType: 'Dev',
 };
 
